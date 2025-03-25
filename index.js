d86148c220cfaa7b4ca7453661c651dec42586c5
Sudan Tandukar <sudan.tandukar@patancollege.edu.np>
2025-03-25 12:07:58 +0000

    api for file analyse was created

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer = require("multer");
 
 var app = express();
+const upload = multer(); 
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,7 +13,16 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post("/api/fileanalyse", upload.single("upfile"),(req,res)=>{
+  const file =req.file;
+  if(!file)return res.status(404).json({error:"No file uploaded!"});
 
+  res.json({
+    name: file.originalname,
+    type: file.mimetype,
+    size: file.size
+  });
+});
 
 
 const port = process.env.PORT || 3000;
